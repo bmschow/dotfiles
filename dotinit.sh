a69192b2109c227ec1441064fbf6234d01466889
bmschow <31709694+bmschow@users.noreply.github.com>
2018-01-15 09:51:40 -0700

    Update dotinit.sh

--- dotinit.sh
+++ dotinit.sh
@@ -3,5 +3,7 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
 config config status.showUntrackedFiles no
 
+echo ". $HOME/.bashrc" >> $HOME/.bash_profile
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
