53c121fab42e163d5436c42cbc3ae21e0907f31b
Brandon Schow <bschow@henselphelps.com>
2018-01-15 09:41:58 -0700

    added config alias to bash_aliases

--- dotinit.sh
+++ dotinit.sh
@@ -2,6 +2,7 @@
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
 config config status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bash_aliases
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
